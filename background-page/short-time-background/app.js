30d5a3e4d7954e5e4792fe09025bef9674346f98
Katal1st <69598081+Katal1st@users.noreply.github.com>
2021-07-23 12:32:04 +0300

    Add files via upload

---
+++ background-page/short-time-background/app.js
@@ -0,0 +1,12 @@
+VANTA.FOG({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 800.00,
+    minWidth: 200.00,
+    highlightColor: 0xe1ab61,
+    midtoneColor: 0x521e19,
+    lowlightColor: 0x7f779d,
+    baseColor: 0x36da7
+})
